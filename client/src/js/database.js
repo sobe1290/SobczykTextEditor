4ea30a859a83692b65ec5f8b7c26f48e60939484
Patrick Sobczyk <sobczyk1290@gmail.com>
2022-11-10 20:00:17 -0800

    adding starter index DB text

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,32 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+
+// Placeholder variables, need to update for this app
+
+export const putDb = async (content) => {
+  console.log('Post to the ase');
+  const todosDb = await openDB('todos', 1);
+  const tx = todosDb.transaction('todos', 'readwrite');
+  const store = tx.objectStore('todos');
+  const request = store.add({ todo: content });
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+
+// Placeholder variables, need to update for this app
+
+export const getDb = async () => {
+  console.log('GET all from the database');
+  const todosDb = await openDB('todos', 1);
+  const tx = todosDb.transaction('todos', 'readonly');
+  const store = tx.objectStore('todos');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};;
 
 initdb();
